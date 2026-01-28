d22e2906d7e123394ea78150b052a27d5e418b1e
Detlef Groth (D7480) <dgroth@mura.de>
2026-01-28 00:09:16 +0000

    adding dart example section

---
+++ tutorial/assets/hello.dart
@@ -0,0 +1,25 @@
+import 'dart:ffi';
+import 'dart:io';
+import 'package:ffi/ffi.dart'; // Import for allocation functions
+// Define the C function signature
+typedef HelloFunc = Pointer<Utf8> Function();
+typedef Hello = Pointer<Utf8> Function();
+
+void main() {
+  print("Greetings from Dart!");
+  // Load the shared library
+  final DynamicLibrary lib = Platform.isWindows
+      ? DynamicLibrary.open('hello.dll')
+      : Platform.isLinux
+          ? DynamicLibrary.open('libhello.so')
+          : DynamicLibrary.open('libhello.dylib');
+
+  // Look up the function
+  final HelloFunc helloPtr = lib
+      .lookup<NativeFunction<HelloFunc>>('Hello_GetMessage')
+      .asFunction();
+
+  // Call the function and get the result
+  final Pointer<Utf8> result = helloPtr();
+  print(result.toDartString());
+}
