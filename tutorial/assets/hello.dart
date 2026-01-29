fb2653ad6810cdded66cc72884715e5eb54376fe
Detlef Groth (D7480) <dgroth@mura.de>
2026-01-29 15:14:49 +0000

    continuing tutorial with dart, v-lang, swig/lua and swig/tcl part

--- tutorial/assets/hello.dart
+++ tutorial/assets/hello.dart
@@ -3,10 +3,9 @@ import 'dart:io';
 import 'package:ffi/ffi.dart'; // Import for allocation functions
 // Define the C function signature
 typedef HelloFunc = Pointer<Utf8> Function();
-//typedef Hello = Pointer<Utf8> Function();
 
 void main() {
-  print("Greetings from Dart2!");
+  print("Greetings from Dart!");
   // Load the shared library
   final DynamicLibrary lib = Platform.isWindows
       ? DynamicLibrary.open('hello.dll')
